94c15231f0b7ac85f86184cbbe4def371cca0620
SonaliGangopadhyay <sonalig.official11@gmail.com>
2020-12-23 00:58:06 -0800

    Check added

--- src/com/abarrantz/snapparc/api/GitDemo.java
+++ src/com/abarrantz/snapparc/api/GitDemo.java
@@ -1,9 +1,13 @@
 package com.abarrantz.snapparc.api;
 
 public class GitDemo {
- public static void main() {
+ public static void main(String[] args) {
 	 int a=2,b=4;
 	 int c=a+b;
-	 System.out.println("the sum is :"+c);
+	 if (c==6) {
+	 System.out.println("this sum is correct:");
  }
+	 else {
+		 System.out.println("The is not correct ");}
+	 }
 }
--- src/com/abarrantz/snapparc/api/TestAdded.java
+++ src/com/abarrantz/snapparc/api/TestAdded.java
@@ -1,5 +1,13 @@
 package com.abarrantz.snapparc.api;
 
 public class TestAdded {
-
-}
+	public static void main(String[] args) {
+		 int a=2,b=4;
+		 int c=a+b;
+		 if (c==6) {
+		 System.out.println("this sum is correct:");
+	 }
+		 else {
+			 System.out.println("The is not correct ");}
+		 }
+	}
