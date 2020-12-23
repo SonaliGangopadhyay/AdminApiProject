ae2cef7a8c668d2602f49f925f55d62f24f0755d
SonaliGangopadhyay <sonalig.official11@gmail.com>
2020-12-23 00:07:36 -0800

    TestAdded class added

--- src/com/abarrantz/snapparc/api/GitDemo.java
+++ src/com/abarrantz/snapparc/api/GitDemo.java
@@ -1,5 +1,9 @@
 package com.abarrantz.snapparc.api;
 
 public class GitDemo {
-
+ public static void main() {
+	 int a=2,b=4;
+	 int c=a+b;
+	 System.out.println("the sum is :"+c);
+ }
 }
---
+++ src/com/abarrantz/snapparc/api/TestAdded.java
@@ -0,0 +1,5 @@
+package com.abarrantz.snapparc.api;
+
+public class TestAdded {
+
+}
